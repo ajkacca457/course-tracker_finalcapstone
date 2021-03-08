2119b2c97323caa4ac3456bfc49e23a2677e3dea
Avijit Karmaker <ajkacca@gmail.com>
2021-03-09 00:20:12 +0200

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 const App = () => (
   <div className="App">
-    <h1 className="bg-primary">Hello world</h1>
+    <h1 className="text-white bg-dark w-25 mx-auto my-5 p-2 text-center rounded head1">
+    <i className="fas fa-chalkboard-teacher mr-2"></i>
+    Course Tracker</h1>
   </div>
 );
 
