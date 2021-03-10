a3b0c218846d559c80ba27a0a8cf84d6f90e25d4
Avijit Karmaker <ajkacca@gmail.com>
2021-03-10 20:28:44 +0200

    add styling for addcourse components

--- src/components/addcourse.js
+++ src/components/addcourse.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Addcourse = () => (
   <div className="logincont">
-    <div className="w-50 bg-white mx-auto p-2 rounded">
+    <div className="bg-white mx-auto p-2 rounded courseitem">
       <h4 className="text-center bg-info py-2 text-white mb-3 rounded">Add new course</h4>
       <form>
         <div className="form-group text-left">
