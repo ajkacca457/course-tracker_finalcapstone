df9f125da6ad55e50dbd587ea4996a37b94773c5
Avijit Karmaker <ajkacca@gmail.com>
2021-03-15 00:53:13 +0200

    add progress bar

--- src/components/course.js
+++ src/components/course.js
@@ -1,19 +1,39 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Course = props => {
   const {
     course: {
       name,
+      number_of_lessons,
+      lesson_completed,
     },
   } = props;
+
+  const percentage = (lesson_completed / number_of_lessons) * 100;
+
   return (
     <div className="course">
-      <div className="course1 card border-2 mb-2 bg-warning">
-        <div className="content d-flex justify-content-between align-items-center p-1">
-          <p className="flex-1">{name}</p>
-          <i className="far fa-2x fa-circle mr-2 text-primary text-center flex-1" />
-          <button type="button" className="btn btn-primary flex-1">view info</button>
+      <div className="course-item card border-2 mb-2 bg-light">
+        <div className="content justify-content-between align-items-center p-1">
+          <p className="name">{name}</p>
+          <div className="progresscont w-25 text-right">
+            <CircularProgressbar
+              value={percentage.toFixed()}
+              text={`${percentage.toFixed()}%`}
+              styles={buildStyles({
+                // Colors
+                pathColor: '#38697a',
+                textColor: '#194775',
+                trailColor: '#D2D2D2',
+                backgroundColor: '#3e98c7',
+              })}
+            />
+          </div>
+          <button type="button" className=" bg-primary rounded border-0 text-white view-btn p-1">view info</button>
         </div>
       </div>
     </div>
@@ -23,6 +43,8 @@ const Course = props => {
 Course.propTypes = {
   course: PropTypes.shape({
     name: PropTypes.string,
+    number_of_lessons: PropTypes.number,
+    lesson_completed: PropTypes.number,
   }).isRequired,
 };
 
