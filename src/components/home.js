697423a424f2d5ae028836d0102957645169c213
Avijit Karmaker <ajkacca@gmail.com>
2021-03-15 22:24:07 +0200

    add additional info to home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Easynav from './easynav';
+import Trackerlogo from '../images/tracker.jpg';
 
 const Home = () => (
   <div className="home">
@@ -12,6 +13,20 @@ const Home = () => (
       Course tracker allows you to track progress of the courses you are taking.
       It is an effective app to make your learning process easy.
     </p>
+
+    <div className="imgcont text-center bg-white py-3">
+      <img src={Trackerlogo} alt="symbol for tracker" className="logo" />
+      <div className="homeinfo">
+        <h4 className="text-left text-dark">Using this App you can:</h4>
+        <ul className="text-left text-dark">
+          <li>Add new courses to your tracker.</li>
+          <li>See progress of each courses.</li>
+          <li>Modify and update your progress of the courses.</li>
+          <li>Once you completed the course you can delete it.</li>
+        </ul>
+      </div>
+    </div>
+
     <Easynav />
   </div>
 );
